0a42932c0027472a8096f0383f153abb5b7d77e0
Christian Schabesberger <chris.schabesberger@mailbox.org>
2019-02-06 16:38:33 +0100

    add load page

---
+++ mockup/script.js
@@ -0,0 +1,11 @@
+
+
+onLoadMoneyClick = function() {
+    document.getElementById("main_page").style.display = 'none';
+    document.getElementById("add_money_page").style.display='block';
+};
+
+onBackButton = function() {
+    document.getElementById("main_page").style.display = 'block';
+    document.getElementById("add_money_page").style.display='none';
+}
