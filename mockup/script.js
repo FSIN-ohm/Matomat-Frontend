cc1f68f0ca4c3b6893a12b8a941c34322acf74f0
Christian Schabesberger <chris.schabesberger@mailbox.org>
2019-02-07 12:03:36 +0100

    add pages

--- mockup/script.js
+++ mockup/script.js
@@ -1,11 +1,50 @@
 
+var keyBuffer = "";
+window.onload = function() {
+    pages = {
+        startPage: document.getElementById("start_page"),
+        mainPage: document.getElementById("main_page"),
+        addMoneyPage: document.getElementById("add_money_page"),
+        thankYouPage: document.getElementById("thank_you_page"),
+    
+        hideAllPages: function() {
+            this.startPage.style.display = 'none';
+            this.mainPage.style.display = 'none';
+            this.addMoneyPage.style.display = 'none';
+            this.thankYouPage.style.display = 'none';
+        },
+    
+        showPage: function(page) {
+            this.hideAllPages();
+            page.style.display = 'block';
+        }
+    };
 
-onLoadMoneyClick = function() {
-    document.getElementById("main_page").style.display = 'none';
-    document.getElementById("add_money_page").style.display='block';
-};
-
-onBackButton = function() {
-    document.getElementById("main_page").style.display = 'block';
-    document.getElementById("add_money_page").style.display='none';
+    pages.showPage(pages.startPage);
 }
+
+onEnterPressed = function(inputString) {
+    pages.showPage(pages.mainPage);
+}
+
+onLogoutButton = function() {
+    pages.showPage(pages.startPage);
+}
+
+onBuyButton = function() {
+    pages.showPage(pages.thankYouPage);
+}
+
+onAddMoneyButton = function() {
+    pages.showPage(pages.addMoneyPage);
+}
+
+document.onkeypress = function(e) {
+    if(e.keyCode != 13) {
+        keyBuffer += String.fromCharCode(e.keyCode);
+    } else {
+        onEnterPressed(keyBuffer);
+        keybuffer = "";
+    }
+}
+
