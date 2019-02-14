7018fc33ae0d6f7a00774036cf14c98be51ee283
Christian Schabesberger <chris.schabesberger@mailbox.org>
2019-02-14 11:57:53 +0100

    genericly generate products

--- mockup/script.js
+++ mockup/script.js
@@ -53,9 +53,52 @@ window.onload = function() {
         }
     }
 
+    products = {
+        productList: [],
+        topRow: document.getElementById("top_row"),
+        bottomRow: document.getElementById("bottom_row"),
+
+        addProduct: function(id, name, imageUrl, price) {
+            var productHtml = `
+                <div class="product">
+                    <img class="product_image" src="${imageUrl}">
+                    <h1>${name}</h1>
+                    <h2>${parseFloat(price).toFixed(2)}€</h2>
+                    <button class="ripple" onmousedown="onProductMouseDown()" onmouseup="onProductMouseUp()" onmousemove="onProductMouseMove()"></button>
+                    <div class="bobel">
+                        <img src="img/beobel.png">
+                        <div class="product_count">0</div>
+                    </div>
+                    <div class="product_remove_button">
+                        <img src="img/trash.png">
+                    </div>
+                </div>
+            `;
+
+            if(this.topRow.childElementCount > this.bottomRow.childElementCount) {
+                this.bottomRow.innerHTML += productHtml;
+            } else {
+                this.topRow.innerHTML += productHtml;
+            }
+        }
+    }
+
     errorScreen.hide();
     loadingScreen.hide();
     pages.showPage(pages.startPage);
+
+    // THIS IS MOCKUP CODE AND NEEDS TO BE REMOVED WHEN IN PRODUCTION
+    products.addProduct(0, "Club Mate", "img/flasche.png", 0.70);
+    products.addProduct(0, "Club Mate Ice Tea", "img/flasche.png", 0.70);
+    products.addProduct(0, "Club Mate Grannat", "img/flasche.png", 0.70);
+    products.addProduct(0, "Club Mate Cola", "img/flasche.png", 0.60);
+    products.addProduct(0, "Club Mate Winter", "img/flasche.png", 0.70);
+    products.addProduct(0, "Wasser", "img/flasche.png", 0.50);
+    products.addProduct(0, "Snickers", "img/flasche.png", 0.20);
+    products.addProduct(0, "Mars", "img/flasche.png", 0.20);
+    products.addProduct(0, "Caffee", "img/flasche.png", 0.20);
+    products.addProduct(0, "Cola Orange", "img/flasche.png", 0.60);
+    products.addProduct(0, "Limo", "img/flasche.png", 0.60);
 }
 
 onEnterPressed = function(inputString) {
