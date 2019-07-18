c3fc185a357181043832a2b3c08ea8761b703c9b
Christian Schabesberger <chris.schabesberger@mailbox.org>
2019-07-18 11:31:14 +0200

    fix emediate logout after entering card on thankyou screen

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const server = 'http://127.0.0.1:8080';
+const server = 'http://127.0.0.1:5000';
 const sessionTimeout = 10000;
 const afterBoughtTimeout = 5000;
 const readAgreementTimeout = 60000;
